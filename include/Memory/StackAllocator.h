1e6bb2637737c73d248403180e1cad7d08940d3e
Emil Grunt <emil.grunt@lumenia.no>
2016-02-18 02:14:28 +0100

    Adds a 'Memory' directory
    The Memory dir is meant to hold memory managers, at present there is
    only the stack allocator. Hopefully more will come.

---
+++ include/Memory/StackAllocator.h
@@ -0,0 +1,30 @@
+#ifndef CAPSTAN_MEMORY_STACK_ALLOCATOR_H
+#define CAPSTAN_MEMORY_STACK_ALLOCATOR_H
+
+
+namespace Capstan
+{
+    struct StackAllocation
+    {
+        void * memory;
+        void * top;
+    };
+
+
+    class StackAllocator
+    {
+    public:
+        StackAllocator (Int64 size);
+        ~StackAllocator (void);
+
+        StackAllocation Allocate (Int64 size);
+        void Free (StackAllocation allocation);
+    private:
+        void * memory;
+        void * top;
+        void * end;
+    };
+}
+
+
+#endif
--- include/memory.h
+++ include/memory.h
@@ -12,19 +12,6 @@ namespace Core
     void * Malloc(size_t size);
     void * Realloc(void * memory, size_t prevSize, size_t newSize);
     Bool32 Free (void * memory);
-
-
-    class StackAllocator
-    {
-    public:
-        StackAllocator (size_t initialSize);
-        void * Alloc (size_t size);
-        void Free (void * memory);
-    private:
-        void * memory;
-        void * top;
-        void * size;
-    };
 }
 }
 
