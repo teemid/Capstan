04c1bce0b0f0d901c33ba2b6226fefc3e7bae407
Emil Grunt <emil.grunt@lumenia.no>
2016-02-18 02:16:36 +0100

    Formatting changes
    src/Win32/Debug.cpp

--- src/Win32/Debug.cpp
+++ src/Win32/Debug.cpp
@@ -8,8 +8,8 @@
 
 namespace Capstan
 {
-    namespace Debug
-    {
+namespace Debug
+{
 #if defined CAPSTAN_DEBUG && CAPSTAN_CONSOLE
 
     void OutputString (char * formattedString, ...)
@@ -32,32 +32,32 @@ namespace Capstan
     }
 
 #endif
-        void Win32HandleError (void)
+    void Win32HandleError (void)
+    {
+        DWORD errorCode = GetLastError();
+        LPTSTR errorMessage = nullptr;
+
+        DWORD success = FormatMessage(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            nullptr,
+            errorCode,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            errorMessage,
+            0,
+            nullptr
+        );
+
+        if (success)
         {
-            DWORD errorCode = GetLastError();
-            LPTSTR errorMessage = nullptr;
-
-            DWORD success = FormatMessage(
-                FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                nullptr,
-                errorCode,
-                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-                errorMessage,
-                0,
-                nullptr
-            );
-
-            if (success)
-            {
-                OutputDebugString(errorMessage);
-                LocalFree(errorMessage);
-            }
-            else
-            {
-                char buffer[250];
-                sprintf_s(buffer, "Could not get error message for %d", errorCode);
-                OutputDebugString(buffer);
-            }
+            OutputDebugString(errorMessage);
+            LocalFree(errorMessage);
+        }
+        else
+        {
+            char buffer[250];
+            sprintf_s(buffer, "Could not get error message for %d", errorCode);
+            OutputDebugString(buffer);
         }
     }
+}
 }
